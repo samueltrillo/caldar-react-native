d5740af9ccadcdb54f1b11a1e307ffd78604d48f
samueltrillo <knubbe10@gmail.com>
2021-03-24 14:19:40 -0300

    First commit

--- App.tsx
+++ App.tsx
@@ -3,9 +3,12 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+        <Text style={styles.headerWhite}>Cald
+          <Text style={styles.headerOrange}>Ar</Text>          
+        </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +18,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  headerWhite: {
+    backgroundColor: '#fc6225',
+    height: 75,
+    fontSize: 45,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: 'white',
+    marginTop: 20
+  },
+  headerOrange: {
+    color: '#3d4855',
   },
 });
