eff178af19e0258f92af540a21354c8102b5cb51
samueltrillo <knubbe10@gmail.com>
2021-03-24 23:38:19 -0300

    Home style added

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+import { styles } from './home-styles';
 
 const Home: React.FC = () => {
 
     return (
-        <View>
-            <Text>
-                Home
-            </Text>
+        <View style={styles.mainContainer}>
+            <View style={styles.centerContainer}>
+                <Text style={styles.title}>
+                    Welcome to
+                </Text>
+                <Text style={styles.orangePart}>
+                    Cald 
+                    <Text style={styles.greyPart}>
+                        Ar
+                    </Text>
+                </Text>
+                <View style={styles.boilerImg}>
+                    <Image 
+                        source={require('../../../images/boiler.png')}
+                        style={styles.image}
+                    />
+                </View>
+            </View>
         </View>
     )
 };
